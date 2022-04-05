2985b01baf5a1cc89c46f9dcc311e11a1cc470bb
Breno Cocheto <breno.cocheto@gmail.com>
2022-04-05 19:34:09 -0300

    feat: Criada classe raiz

---
+++ refactored/Game.py
@@ -0,0 +1,8 @@
+class Game:
+
+    def __init__(self, player1Name, player2Name):
+        self.player1Name = player1Name
+        self.player2Name = player2Name
+        self.p1points = 0
+        self.p2points = 0
+

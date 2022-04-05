b847400ceeed58ca4285f3c3d64a55140d33f38c
Breno Cocheto <breno.cocheto@gmail.com>
2022-04-05 19:42:35 -0300

    feat: Criados getters e setters, alterados nomes das variaveis para uma melhor compreensão

--- refactored/Game.py
+++ refactored/Game.py
@@ -1,8 +1,34 @@
 class Game:
 
     def __init__(self, player1Name, player2Name):
-        self.player1Name = player1Name
-        self.player2Name = player2Name
-        self.p1points = 0
-        self.p2points = 0
+        self.playerOneName = playerOneName
+        self.playerTwoName = playerTwoName
+        self.playerOnePoints = 0
+        self.playerTwoPoints = 0
 
+    def setPlayerOneName (self,name):
+        self.playerOneName = name
+    
+    def setPlayerTwoName (self,name):
+        self.playerTwoName = name
+
+    def setPlayerOnePoints(self,points):
+        self.playerOnePoints += points
+
+    def setPlayerTwoPoints(self,points):
+        self.playerTwoPoints += points
+
+    def getPlayerOneName (self):
+        return self.playerOneName
+    
+    def getPlayerTwoName (self):
+        return self.playerTwoName
+
+    def getPlayerOnePoints(self):
+        return self.playerOnePoints
+
+    def getPlayerTwoPoints(self):
+        return self.playerTwoPoints
+
+    
+    

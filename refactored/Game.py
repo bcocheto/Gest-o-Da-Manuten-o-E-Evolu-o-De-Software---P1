698e1a54c3799e055a26950a52f2f866bda1b503
Breno Cocheto <breno.cocheto@gmail.com>
2022-04-05 20:27:52 -0300

    feat: criada função para retornar player com maior pontuação

--- refactored/Game.py
+++ refactored/Game.py
@@ -2,9 +2,16 @@ from .Player import Player
 
 class Game:
 
-    def __init__(self):
-        self._player = Player
+    def __init__(self, score, playerOne, playerTwo):
+        self._playerOne = Player # input do usuário
+        self._playerTwo = Player #input do usuário
+        self._score = score
 
     def setGainPoints(self, points):
         self._player.setPlayerPoints(points)
+
+    def score(self):
+
+    def getPlayerWithTheMostPoints(self):
+        return self._playerOne.getPlayerPoints() if self._playerOne.getPlayerPoints()>self._playerTwo.getPlayerPoints() else self._playerTwo.getPlayerPoints()
         
